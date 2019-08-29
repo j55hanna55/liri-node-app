412466c9b0838a60f8d2288eae7ec339442432e3
Jacob Hanna <jacob@Jacobs-MacBook-Pro.local>
2019-08-28 21:01:45 -0700

    creating my files

---
+++ liri.js
@@ -0,0 +1,9 @@
+require("dotenv").config();
+spotify-this-song,"I Want it That Way"
+var fs = require("fs");
+var request = require("request");
+var axios = require("axios");
+var spotify = require("node-spotify-api");
+var moment = require("moment");
+var spotify = new spotify(keys.spotify);
+var now = new Date();
